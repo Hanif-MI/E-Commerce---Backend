cc68a9909d0719d5b884eb142a256dd659fe1a71
Hanif Shaikh <hanif.shaikh@mindinventory.com>
2025-04-18 16:14:51 +0530

    refactor database configuration to remove direct config import

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,10 @@
 import { Sequelize } from "sequelize";
 import * as fs from "fs";
 import * as dotenv from "dotenv";
-import config from "./config";
 
 dotenv.config();
 
 const useSSL = process.env.TIDB_ENABLE_SSL === "true";
-
-const dbConfig = config[process.env.NODE_ENV || "development"];
 
 const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: "mysql",
